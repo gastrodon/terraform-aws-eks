fc885b20d00cd1e05a672c674b1e787df34e30fb
Eva <mail@gastrodon.io>
2022-11-17 09:14:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_cloudwatch_log_group" "this" {
 
 module "kms" {
   source  = "github.com/gastrodon/terraform-aws-kms"
-  version = "1.2.2" # Note - be mindful of Terraform/provider version compatibility between modules
+  version = "master" # Note - be mindful of Terraform/provider version compatibility between modules
 
   create = local.create && var.create_kms_key
 
